08ea887c5b6b3b60f5f158056e3dda2a575744e9
Wessel <w.vanderplas@cheil.com>
2019-05-04 11:27:13 +0200

    added animations to info component

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import infoShoe from '../images/info-shoe.png';
-import markShoe from '../images/mark-shoe.png';
-import travis1 from '../images/travis-1.png';
-import travis2 from '../images/travis-2.png';
-import travis3 from '../images/travis-3.png';
-import galleryShoe from '../images/gallery-shoe.png';
-import smiley from '../images/smiley.png';
-import cactus from '../images/cactus-illustration.png';
-import momText from '../images/mom.png';
-import sand from '../images/sand.png';
-import arrowLeft from '../images/arrow-left.png';
-import arrowRight from '../images/arrow-right.png';
+import React, { Component } from "react";
+import infoShoe from "../images/info-shoe.png";
+import markShoe from "../images/mark-shoe.png";
+import travis1 from "../images/travis-1.png";
+import travis2 from "../images/travis-2.png";
+import travis3 from "../images/travis-3.png";
+import galleryShoe from "../images/gallery-shoe.png";
+import smiley from "../images/smiley.png";
+import cactus from "../images/cactus-illustration.png";
+import momText from "../images/mom.png";
+import sand from "../images/sand.png";
+import arrowLeft from "../images/arrow-left.png";
+import arrowRight from "../images/arrow-right.png";
+
+import Roll from "react-reveal/Roll";
+import Zoom from "react-reveal/Zoom";
+import RubberBand from "react-reveal/RubberBand";
 
 class Info extends Component {
   render() {
@@ -18,16 +22,28 @@ class Info extends Component {
       <section id="info" className="section">
         <div className="container info">
           <div className="section__intro d-flex flex-column align-items-center text-center">
-            <p className="tagline">INFO</p>
+            <Roll>
+              <p className="tagline">INFO</p>
+            </Roll>
             <h2 className="title">TRAVIS SCOTT X AIR JORDAN 1</h2>
-            <p>Lorem ipsum dolor st amet, consectuer adeputhtunf wijidj disj </p>
+            <p>
+              Lorem ipsum dolor st amet, consectuer adeputhtunf wijidj disj{" "}
+            </p>
           </div>
           <div className="row">
             <div className="info__shoe col-sm d-flex justify-content-center">
-              <img className="infoShoe" src={infoShoe} />
-              <img src={markShoe} className="shoe-mark shoe-mark--1" />
-              <img src={markShoe} className="shoe-mark shoe-mark--2" />
-              <img src={markShoe} className="shoe-mark shoe-mark--3" />
+              <Zoom top>
+                <img className="infoShoe" src={infoShoe} />
+              </Zoom>
+              <RubberBand>
+                <img src={markShoe} className="shoe-mark shoe-mark--2" />
+              </RubberBand>
+              <RubberBand>
+                <img src={markShoe} className="shoe-mark shoe-mark--1" />
+              </RubberBand>
+              <RubberBand>
+                <img src={markShoe} className="shoe-mark shoe-mark--3" />
+              </RubberBand>
               <div className="d-none d-sm-block">
                 <p className="shoe-tips shoe-tips--1">SECTRET STASH</p>
                 <img src={arrowRight} className="shoe-arrow shoe-arrow--1" />
@@ -38,22 +54,44 @@ class Info extends Component {
               </div>
             </div>
           </div>
+
           <div className="row d-flex justify-content-end my-3">
-            <div class="col-sm info__content">
-              <h4>LIMITED TRAVIS SCOTT X AIR JORDAN 1 POSTER</h4>
-              <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div className="col-sm info__content">
+              <Roll>
+                <h4>LIMITED TRAVIS SCOTT X AIR JORDAN 1 POSTER</h4>
+                <p className="mt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </Roll>
             </div>
           </div>
 
           <div className="row justify-content-end">
             <div className="info__gallery">
-              <img src={galleryShoe} className="info__gallery__shoe d-none d-sm-block" />
+              <img
+                src={galleryShoe}
+                className="info__gallery__shoe d-none d-sm-block"
+                alt="none"
+              />
               <div className="info__gallery__mobile-composition">
-                <img src={smiley} className="gallery_obj gallery_obj--1" />
-                <img src={travis1} className="gallery_obj gallery_obj--2" />
-                <img src={travis2} className="gallery_obj gallery_obj--3" />
-                <img src={travis3} className="gallery_obj gallery_obj--4" />
-                <img src={cactus} className="gallery_obj gallery_obj--5" />
+                <Roll>
+                  <img src={smiley} className="gallery_obj gallery_obj--1" />
+                </Roll>
+                <Roll>
+                  <img src={travis1} className="gallery_obj gallery_obj--2" />
+                </Roll>
+                <Roll>
+                  <img src={travis2} className="gallery_obj gallery_obj--3" />
+                </Roll>
+                <Roll>
+                  <img src={travis3} className="gallery_obj gallery_obj--4" />
+                </Roll>
+                <RubberBand>
+                  <img src={cactus} className="gallery_obj gallery_obj--5" />
+                </RubberBand>
                 <img src={momText} className="gallery_obj gallery_obj--6" />
               </div>
             </div>
@@ -61,16 +99,23 @@ class Info extends Component {
           <div className="row">
             <div className="info__description col">
               <h4>LIMITED TRAVIS SCOTT X AIR JORDAN 1 POSTER</h4>
-              <p>Phasellus gravida semper nisi. Fusce a quam. Integer tincidunt. Phasellus accumsan cursus velit.
-              Suspendisse potenti. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Phasellus a est. Vestibulum ullamcorper mauris at ligula. Etiam rhoncus.
-              In ut quam vitae odio lacinia tincidunt. Nunc nonummy metus. Vivamus quis mi. Sed libero.
+              <p>
+                Phasellus gravida semper nisi. Fusce a quam. Integer tincidunt.
+                Phasellus accumsan cursus velit. Suspendisse potenti. Proin
+                sapien ipsum, porta a, auctor quis, euismod ut, mi. Phasellus a
+                est. Vestibulum ullamcorper mauris at ligula. Etiam rhoncus. In
+                ut quam vitae odio lacinia tincidunt. Nunc nonummy metus.
+                Vivamus quis mi. Sed libero.
               </p>
-              <img src={galleryShoe} className="info__gallery__shoe d-block d-sm-none" />
+              <img
+                src={galleryShoe}
+                className="info__gallery__shoe d-block d-sm-none"
+              />
             </div>
           </div>
         </div>
-      </section >
-    )
+      </section>
+    );
   }
 }
 
